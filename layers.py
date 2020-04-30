fix adjacency computation

layers.py
@@ -97,24 +97,26 @@ class Adjacency(Layer):
         adj_2 = self._learn_adjacencies(adj_2, node_vec)
         
         # flatten
-        adj_0 = tf.reshape(adj_0, [-1])
-        adj_1 = tf.reshape(adj_1, [-1])
-        adj_2 = tf.reshape(adj_2, [-1])
+        # print("ajd_0.shape before reshape", adj_0.shape)
+        # adj_0 = tf.reshape(adj_0, [-1])
+        # print("ajd_0.shape", adj_0.shape)
+        # adj_1 = tf.reshape(adj_1, [-1])
+        # adj_2 = tf.reshape(adj_2, [-1])
 
         # forward pass
-        adj_0 = tf.matmul(adj_0 * self.w0_1)
+        adj_0 = tf.matmul(adj_0, self.w0_1)
         adj_0 = tf.nn.relu(adj_0)
-        adj_0 = tf.matmul(adj_0 * self.w0_2)
+        adj_0 = tf.matmul(adj_0, self.w0_2)
         adj_0 = tf.nn.relu(adj_0)
 
-        adj_1 = tf.matmul(adj_1 * self.w1_1)
+        adj_1 = tf.matmul(adj_1, self.w1_1)
         adj_1 = tf.nn.relu(adj_1)
-        adj_1 = tf.matmul(adj_1 * self.w1_2)
+        adj_1 = tf.matmul(adj_1,  self.w1_2)
         adj_1 = tf.nn.relu(adj_1)
 
-        adj_2 = tf.matmul(adj_2 * self.w2_1)
+        adj_2 = tf.matmul(adj_2,  self.w2_1)
         adj_2 = tf.nn.relu(adj_2)
-        adj_2 = tf.matmul(adj_2 * self.w2_2)
+        adj_2 = tf.matmul(adj_2, self.w2_2)
         adj_2 = tf.nn.relu(adj_2) 
 
         # reshape back to original shape
@@ -246,9 +248,9 @@ class GraphOperator(Layer):
         # calculate powers of the adjacency matrix
         # NOTE: A^0 = I
         A0 = tf.eye(num_rows=int(n), num_columns=int(n))
-        A1 = tf.matmul(adj, adj)
+        A1 = adj
         A2 = tf.matmul(A1, adj)
-        
+
         return tf.stack([A0, A1, A2])
 
     def compute_output_shape(self, input_shape=(50, 50)):